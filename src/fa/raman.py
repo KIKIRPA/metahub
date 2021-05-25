9b9b2f2564614b730a6f37a89f9e12328a855b36
Emmanuel Di Pretoro <emmanuel.dipretoro@kikirpa.be>
2021-05-25 11:19:58 +0200

    Adding a first set of classes to represent RAMAN data

---
+++ src/fa/raman.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel
+
+
+class Grating(BaseModel):
+    grating_type: str
+    grating_density: int
+
+
+class LaserPower(BaseModel):
+    laser_power_at_sample: float
+    neutral_density_filtering: float
+
+
+class RejectionFilters(BaseModel):
+    filter_type: str
+    cutoff_frequency: float
+
+
+class SpectralRange(BaseModel):
+    spectral_range_low: float
+    spectral_range_high: float
+
+
+class Raman(BaseModel):
+    instrument: str
+    software: str
+    detector_type: str
+    instrument_class: str
+    accessory: str
+    excitation_source: float
+    laser_power: LaserPower
+    spectral_range: SpectralRange
+    filters: RejectionFilters
+    grating: Grating
+    resolution: float
+    laser_defocus: bool
+    data_collection: str
+    integration_time: float
+    accumulations: int
+    objective: str
+    spot_size: float
+    confocality: bool
+    polarization: str
+    baseline_correction: bool
+    fluorescence_correction: bool
+    cosmic_ray_removal: bool
+    detector_binning: bool
+    other_data_processing: str
