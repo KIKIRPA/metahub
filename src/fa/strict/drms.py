d20ccf74f945b5872990150aea71546b3ffb5e1d
Emmanuel Di Pretoro <emmanuel.dipretoro@kikirpa.be>
2021-05-25 13:53:52 +0200

    Adding a draft class to represent DRMS data

---
+++ src/fa/strict/drms.py
@@ -0,0 +1,10 @@
+from measurement import Measurement
+
+
+class DRMS(Measurement):
+    instrument: str
+    software: str
+    drill_type: str
+    radius: float
+    rotation_speed: int
+    penetration_rate: int
