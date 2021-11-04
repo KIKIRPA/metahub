e0ee8852213f311a78548170fbbba69d63f5439c
Unknown <wfremout+github@gmail.com>
2021-11-04 16:45:02 +0100

    Add endpoint to json-schemas

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
 
-from routers import measurements
+from routers import measurements, schemas
 
 # Main API application
 app = FastAPI()
 # Adding some routes to our main application
 app.include_router(measurements.router)
+app.include_router(schemas.router)
