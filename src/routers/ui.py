c0bb1d61c008300c777e0011fe968f1a67fec82a
Unknown <wfremout+github@gmail.com>
2021-11-18 11:56:55 +0100

    Create single endpoint for all layouts

--- src/routers/ui.py
+++ src/routers/ui.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from typing import Optional
 
 from models.measurement import Measurement
 from models.drms import DRMS
@@ -18,17 +20,13 @@ router.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@router.get("/alpaca", response_class=HTMLResponse)
-async def show_measurements_form_alpaca(request: Request):
-    """Displaying form using alpaca.
+@router.get("/form", response_class=HTMLResponse)
+async def show_form(request: Request, layout: Optional[str] = "jsoneditor"):
+    """Displaying document input form
     """
+    
+    if layout not in ["jsoneditor", "alpaca"]:
+        raise HTTPException(status_code=404, detail="Layout does not exist")
+    
     schema = Measurement.schema_json(indent=4)
-    return templates.TemplateResponse("alpaca.html", {"request": request, "schema": schema})
-
-
-@router.get("/jsoneditor", response_class=HTMLResponse)
-async def show_measurements_form_jsoneditor(request: Request):
-    """Displaying form using json-editor.
-    """
-    schema = Measurement.schema_json(indent=4)
-    return templates.TemplateResponse("jsoneditor.html", {"request": request, "schema": schema})
+    return templates.TemplateResponse(layout+".html.jinja", {"request": request, "schema": schema})
