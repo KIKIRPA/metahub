7632415121415d9e4697fbb7919a4b4e0d56b109
Unknown <wfremout+github@gmail.com>
2021-11-26 17:09:14 +0100

    Improve DRMS model

--- src/models/drms.py
+++ src/models/drms.py
@@ -1,10 +1,30 @@
+from enum import Enum
+from typing import Set, Optional
+
+from pydantic import BaseModel, Field
 from .measurement import Measurement
 
 
+class DRMSParameters(BaseModel):
+    instrument: Optional[str]
+    software: Set[str]
+    drill_type: Optional[str]
+    radius: Optional[float]
+    rotation_speed: Optional[int]
+    penetration_rate: Optional[int]
+    
+    class Config:
+        title = "Measurement Parameters"
+
+
+class DRMSResults(BaseModel):
+    comments: Optional[str] = Field(None, title='Comments', description='Comments with regards to generated research data')
+
+    class Config:
+        title = "Measurement Results"
+
+
 class DRMS(Measurement):
-    instrument: str
-    software: str
-    drill_type: str
-    radius: float
-    rotation_speed: int
-    penetration_rate: int
+    document_type: str = Field("Drilling resistance measurement system", title='Document type', const=True) #OVERRIDE FROM DOCUMENT
+    measurement_parameters: Optional[DRMSParameters] = Field(None)
+    results: Optional[DRMSResults] = Field(None)
