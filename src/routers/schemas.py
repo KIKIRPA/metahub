7a88b90c9ea68d163ab4474a2fc926997812fbbf
Unknown <wfremout+github@gmail.com>
2021-11-27 21:01:39 +0100

    Test proof-of-concept schema templates

--- src/routers/schemas.py
+++ src/routers/schemas.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException, Path
 
-from models.document_types import document_types
+from models.document_types import document_types, test_document_templates
 
 # Creating a FastAPI router, meaning a set of routes that can be included later
 # in the FastAPI application
@@ -10,9 +10,26 @@ router = APIRouter(
 )
 
 
+def merge(source, destination):
+    """
+    Recursively merge two dicts
+    """
+    for key, value in destination.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = source.setdefault(key, {})
+            merge(value, node)
+        else:
+            source[key] = value
+
+    return source
+
+
 @router.get("/{document_type}")
-async def read_measurement_schema(document_type: str = Path(None, description="The type of report or measurement")):
-    """Displaying json-schema.
+async def read_measurement_schema(
+    document_type: str = Path(None, description="The type of report or measurement")):
+    """
+    Displaying json-schema.
     """
 
     if document_type not in document_types:
@@ -20,3 +37,22 @@ async def read_measurement_schema(document_type: str = Path(None, description="T
 
     return document_types[document_type]["model"].schema()
 
+
+@router.get("/{document_type}/{template}")
+async def read_measurement_schema(
+    document_type: str = Path(None, description="The type of report or measurement"),
+    template: str = Path(None, description="Schema template to be applied")
+):
+    """
+    Displaying json-schema with an applied schema template.
+    """
+
+    if document_type not in document_types:
+        raise HTTPException(status_code=404, detail="Document type does not exist")
+
+    if template not in test_document_templates:
+        raise HTTPException(status_code=404, detail="Template type does not exist (for the given document type)")
+
+    schema = merge(document_types[document_type]["model"].schema(), test_document_templates[template]["template"])
+
+    return schema
