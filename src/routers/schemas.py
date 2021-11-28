1251aeed780486ad93248fdab6afecbd4d5231f9
Unknown <wfremout+github@gmail.com>
2021-11-28 21:48:01 +0100

    Fetch schema templates in schemas endpoints

--- src/routers/schemas.py
+++ src/routers/schemas.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter, HTTPException, Path
+from functools import lru_cache
+from config import Settings
+
+from fastapi import APIRouter, HTTPException, Path, Depends
+import motor.motor_asyncio
 
 from models.document_types import document_types, test_document_templates
 
@@ -8,6 +12,10 @@ router = APIRouter(
     prefix="/schemas",
     tags=["schemas"]
 )
+
+@lru_cache()
+def get_settings():
+    return Settings()
 
 
 def merge(source, destination):
@@ -41,17 +49,22 @@ async def read_measurement_schema(
 @router.get("/{document_type}/{template}")
 async def read_measurement_schema(
         document_type: str = Path(None, description="The type of report or measurement"),
-        template: str = Path(None, description="Schema template to be applied")):
+        template: str = Path(None, description="Schema template to be applied"),
+        config: Settings = Depends(get_settings)):
     """
     Displaying json-schema with an applied schema template.
     """
 
+
     if document_type not in document_types:
         raise HTTPException(status_code=404, detail="Document type does not exist")
 
-    if template not in test_document_templates:
+    client = motor.motor_asyncio.AsyncIOMotorClient(config.mongo_conn_str)
+    db = client[config.mongo_db]
+
+    if (response := await db[config.templates_collection].find_one({"alias": template, "schemas": document_type})) is None:
         raise HTTPException(status_code=404, detail="Template type does not exist (for the given document type)")
 
-    schema = merge(document_types[document_type]["model"].schema(), test_document_templates[template]["template"])
+    schema = merge(document_types[document_type]["model"].schema(), response["template"])
 
     return schema
