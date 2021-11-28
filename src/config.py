d613ea3fdafa49efc07fdbbbfcbe5d1954630ca6
Unknown <wfremout+github@gmail.com>
2021-11-28 14:33:57 +0100

    Set up settings and dotenv

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    app_name: str = "Meta"
+    admin_email: str = "hescida@kikirpa.be"
+    mongo_conn_str: str = "mongodb://kikirpa:hescida@localhost:27017/"
+    mongo_db: str = "meta"
+
+    class Config:
+        env_file = ".env"
