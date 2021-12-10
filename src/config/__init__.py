e84b8c568323d474aea9bb554f87b3392bd2d230
Unknown <wfremout+github@gmail.com>
2021-12-10 16:36:53 +0100

    Add activity types config

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -1,2 +1,3 @@
 from .settings import settings
 from .document_types import document_types
+from .activity_types import activity_types
---
+++ src/config/activity_types.py
@@ -0,0 +1,15 @@
+import models.activities
+
+
+activity_types = {
+    "intervention_file": {
+        "alias": "intervention_file",
+        "name": "Intervention file",
+        "model": models.activities.intervention_file
+    },
+    "project": {
+        "alias": "project",
+        "name": "¨Project",
+        "model": models.activities.project
+    }
+}
