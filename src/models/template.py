187dbe77d5db8538f9d412713fb17a5a251a3ef7
Unknown <wfremout+github@gmail.com>
2021-12-15 21:45:32 +0100

    Add pydantic model for schemas and templates

---
+++ src/models/template.py
@@ -0,0 +1,63 @@
+from enum import Enum
+from typing import Optional
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+from bson import ObjectId
+
+
+class PyObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid objectid")
+        return ObjectId(v)
+
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+
+class Category(str, Enum):
+    ACTIVITY = 'Activity'
+    COLLECTION = 'Collection'
+    SAMPLE = 'Sample'
+    DOCUMENT = 'Document'
+
+
+class Template(BaseModel): # unique index on (category, schema_id and template_id)
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    category: Category = Field(..., 
+        description="Category for which the template will be used in the application") 
+    schema_id: str = Field(..., 
+        description="Short identifier for the schema (a-z, 0-9, -, _)",
+        min_length=1,
+        max_length=15,
+        regex='^[a-z0-9-_]*$') 
+    template_id: str = Field("_default", 
+        description="Short identifier for the template (a-z, 0-9, -, _)",
+        min_length=1,
+        max_length=15,
+        regex='^[a-z0-9-_]*$')
+    short_name: str = Field(...,
+        description="Name for the schema or template",
+        min_length=1,
+        max_length=15)
+    long_name: str = Field(..., 
+        description="Name for the schema or template",
+        min_length=1,
+        max_length=100)
+    selectable: bool = Field(False, description="Allow users to select this template")
+    json_schema: str = Field(..., description="JSON schema text")
+    created_timestamp: datetime = Field(...)
+    created_by_user: str = Field(...)
+    modified_timestamp: Optional[datetime] = Field(None)
+    modified_by_user: Optional[str] = Field(None)
+
+    class Config:
+        json_encoders = {ObjectId: str}
+
