2211f34efc96cb25abbf1492d3757aeb968f7820
Unknown <wfremout+github@gmail.com>
2021-12-15 21:51:58 +0100

    Rename schema router

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from routers import api, schemas, ui
+from routers import api, schema, ui
 
 # Main API application
 app = FastAPI()
@@ -10,5 +10,5 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Adding some routes to our main application
 app.include_router(api.router)
-app.include_router(schemas.router)
+app.include_router(schema.router)
 app.include_router(ui.router)
