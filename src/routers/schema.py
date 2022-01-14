7ca02ff52d553787413b097b8ba8b939ec9c9d53
Unknown <wfremout+github@gmail.com>
2022-01-14 15:25:47 +0100

    Rewrite schema router (WiP)

--- src/routers/schema.py
+++ src/routers/schema.py
@@ -3,7 +3,8 @@ from mergedeep import merge
 from fastapi import APIRouter, HTTPException, Path
 from motor.motor_asyncio import AsyncIOMotorClient
 
-import config
+from models import Resource, Activity, Document
+
 
 # Creating a FastAPI router, meaning a set of routes that can be included later
 # in the FastAPI application
@@ -13,48 +14,65 @@ router = APIRouter(
 )
 
 
-@router.get("/activity/{activity_type}")
-async def read_measurement_schema(
-        activity_type: str = Path(None, description="The type of activity")):
+
+@router.get("/{resource}")
+async def get_schema_by_resource(
+        resource: Resource = Path(None, description="The type of resource")):
     """
-    Returning a json-schema for the activity.
+    Returning a json-schema for the resource.
     """
-
-    if activity_type not in config.activity_types:
-        raise HTTPException(status_code=404, detail="Activity type does not exist")
-
-    return config.activity_types[activity_type]["model"].schema()
-
-
-@router.get("/document/{document_type}")
-async def read_measurement_schema(
-        document_type: str = Path(None, description="The type of report or measurement")):
-    """
-    Returning the json-schema for the document.
-    """
-
-    if document_type not in config.document_types:
-        raise HTTPException(status_code=404, detail="Document type does not exist")
-
-    return config.document_types[document_type]["model"].schema()
+    try: 
+        model = resource.value.capitalize()
+        schema = globals()[model].schema()
+    except:
+        raise HTTPException(status_code=404, detail="schema not found")
+    return schema
 
 
-@router.get("/document/{document_type}/{template}")
-async def read_measurement_schema(
-        document_type: str = Path(None, description="The type of report or measurement"),
-        template: str = Path(None, description="Schema template to be applied")):
-    """
-    Returning the json-schema for the document with an applied schema template.
-    """
 
-    if document_type not in config.document_types:
-        raise HTTPException(status_code=404, detail="Document type does not exist")
-    schema = config.document_types[document_type]["model"].schema()
 
-    client = AsyncIOMotorClient(config.settings.mongo_conn_str)
-    db = client[config.settings.mongo_db]
+# @router.get("/activity/{activity_type}")
+# async def read_measurement_schema(
+#         activity_type: str = Path(None, description="The type of activity")):
+#     """
+#     Returning a json-schema for the activity.
+#     """
 
-    if (response := await db[config.settings.templates_collection].find_one({"alias": template, "schemas": document_type})) is None:
-        raise HTTPException(status_code=404, detail="Template type does not exist (for the given document type)")
+#     if activity_type not in config.activity_types:
+#         raise HTTPException(status_code=404, detail="Activity type does not exist")
 
-    return merge({}, schema, response["template"])
+#     return config.activity_types[activity_type]["model"].schema()
+
+
+# @router.get("/document/{document_type}")
+# async def read_measurement_schema(
+#         document_type: str = Path(None, description="The type of report or measurement")):
+#     """
+#     Returning the json-schema for the document.
+#     """
+
+#     if document_type not in config.document_types:
+#         raise HTTPException(status_code=404, detail="Document type does not exist")
+
+#     return config.document_types[document_type]["model"].schema()
+
+
+# @router.get("/document/{document_type}/{template}")
+# async def read_measurement_schema(
+#         document_type: str = Path(None, description="The type of report or measurement"),
+#         template: str = Path(None, description="Schema template to be applied")):
+#     """
+#     Returning the json-schema for the document with an applied schema template.
+#     """
+
+#     if document_type not in config.document_types:
+#         raise HTTPException(status_code=404, detail="Document type does not exist")
+#     schema = config.document_types[document_type]["model"].schema()
+
+#     client = AsyncIOMotorClient(config.settings.mongo_conn_str)
+#     db = client[config.settings.mongo_db]
+
+#     if (response := await db[config.settings.templates_collection].find_one({"alias": template, "schemas": document_type})) is None:
+#         raise HTTPException(status_code=404, detail="Template type does not exist (for the given document type)")
+
+#     return merge({}, schema, response["template"])
