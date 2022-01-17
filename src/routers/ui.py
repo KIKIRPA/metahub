d76668fca3da424f9383ab31a6ad2fb91a7dc760
Unknown <wfremout+github@gmail.com>
2022-01-17 16:36:05 +0100

    Add routes in UI for getting template by its keys

--- src/routers/ui.py
+++ src/routers/ui.py
@@ -9,6 +9,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 import config
 import models
+import crud
 
 # Creating a FastAPI router, meaning a set of routes that can be included later
 # in the FastAPI application
@@ -20,6 +21,10 @@ router = APIRouter(
 templates = Jinja2Templates(directory="templates")
 document_types_list = json.dumps([{"alias": k, "short": v["short"]} for k, v in config.document_types.items()])
 activity_types_list = json.dumps([{"alias": k, "name": v["name"]} for k, v in config.activity_types.items()])
+
+# Creating a MongoDB client and connect to the relevant collections
+client = AsyncIOMotorClient(config.settings.mongo_conn_str)
+db = client[config.settings.mongo_db]
 
 
 @router.get("/templates", response_class=HTMLResponse)
@@ -55,6 +60,56 @@ def show_template_form_with_id(
         "id": template_id
     })
 
+
+@router.get("/templates/{resource}/{category}/{template}", response_class=HTMLResponse)
+async def show_template_form_with_keys(
+        request: Request,
+        resource: models.Resource = Path(None, description="Resource of the data described in the template"),
+        category: str = Path(None, description="Category of the data described in the template"),
+        template: str = Path("_default", description="Template name")):
+    """
+    Displaying the form for the template with given resource, category and template 
+    """
+    try:
+        response = await crud.template.get_by_keys(
+            collection=db[config.settings.templates_collection], 
+            resource=resource,
+            category=category,
+            template=template)
+    except crud.NoResultsError:
+        raise HTTPException(status_code=404, detail="template not found")
+    except BaseException as err:
+        raise HTTPException(status_code=400, detail=err)
+
+    return templates.TemplateResponse("template_form.html.jinja", {
+        "request": request,
+        "schema": models.TemplateUpdate.schema_json(),
+        "id": response["id"]})
+
+
+@router.get("/templates/{resource}/{category}", response_class=HTMLResponse)
+async def show_default_template_form_with_keys(
+        request: Request,
+        resource: models.Resource = Path(None, description="Resource of the data described in the template"),
+        category: str = Path(None, description="Category of the data described in the template")):
+    """
+    Displaying the default form with given resource and category
+    """
+    try:
+        response = await crud.template.get_by_keys(
+            collection=db[config.settings.templates_collection], 
+            resource=resource,
+            category=category,
+            template="_default")
+    except crud.NoResultsError:
+        raise HTTPException(status_code=404, detail="template not found")
+    except BaseException as err:
+        raise HTTPException(status_code=400, detail=err)
+
+    return templates.TemplateResponse("template_form.html.jinja", {
+        "request": request,
+        "schema": models.TemplateUpdate.schema_json(),
+        "id": response["id"]})
 
 
 
