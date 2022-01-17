4405d89aafe1a9566564be8be564b2c1c88b3948
Unknown <wfremout+github@gmail.com>
2022-01-17 15:53:56 +0100

    Add custom template CRUD method to get template by its keys

--- src/crud/crud_templates.py
+++ src/crud/crud_templates.py
@@ -1,4 +1,23 @@
-from crud.base import CRUDBase
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+from crud.base import CRUDBase, NoResultsError, translate_id
+import models
 
 
-template = CRUDBase()
+class CRUDTemplate(CRUDBase):
+    async def get_by_keys(
+            self, 
+            collection: AsyncIOMotorCollection, 
+            *,
+            resource: models.Resource,
+            category: str,
+            template: str = "_default") -> dict:
+        result = await collection.find_one({
+            "resource": resource.value, 
+            "category": category, 
+            "template": template})
+        if result is None: raise NoResultsError
+        return translate_id(result)
+
+
+template = CRUDTemplate()
