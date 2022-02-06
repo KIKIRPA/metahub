849f37a75b1bd96bd1c31b04089bbd9f3443af9a
kobbejager <wfremout+github@gmail.com>
2022-02-06 14:41:36 +0100

    Add field $schema in instances

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -1,6 +1,6 @@
 from typing import Optional, List, Set
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, HttpUrl, Field
 
 from models.common import IdBaseModel, LoggingBaseModel, QueryParameters, Terms, Contributor
 
@@ -11,16 +11,7 @@ class File(BaseModel):
 
 
 class DatasetUpdate(BaseModel):
-    category: str = Field(..., 
-        description="Category identifier (a-z, 0-9, -, _)",
-        min_length=1,
-        max_length=20,
-        regex='^[a-z0-9-_]*$') 
-    template: Optional[str] = Field(None, 
-        description="Template identifier (a-z, 0-9, -, _)",
-        min_length=1,
-        max_length=20,
-        regex='^[a-z0-9-_]*$')
+    described_by: HttpUrl = Field(..., alias="$schema")
     project_id: str = Field(..., title='Project Id')
     object_id: Optional[int] = Field(None, description="Object number to which this dataset belongs")
     contributors: Optional[Set[Contributor]] = Field(...)
--- src/models/project.py
+++ src/models/project.py
@@ -1,24 +1,14 @@
-from enum import Enum
 from typing import Optional, List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, HttpUrl, Field
 
 from models.common import IdBaseModel, LoggingBaseModel, QueryParameters, Contributor, Unit, State, Terms
 
 
 class ProjectUpdate(BaseModel):
+    described_by: HttpUrl = Field(..., alias="$schema") 
     project_code: str = Field(..., description='Project code (file number, acronym...)')
     unit: Unit = Field(...)
-    category: str = Field(..., 
-        description="Category identifier (a-z, 0-9, -, _)",
-        min_length=1,
-        max_length=20,
-        regex='^[a-z0-9-_]*$') 
-    template: Optional[str] = Field(None, 
-        description="Template identifier (a-z, 0-9, -, _)",
-        min_length=1,
-        max_length=20,
-        regex='^[a-z0-9-_]*$')
     subject: Optional[str] = Field(None, description='Subject of the project (e.g. project name or object title)')
     contributors: Optional[List[Contributor]] = Field(None, unique=True)
     state: State = Field(...)
