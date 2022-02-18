665e2992112e32b0ceaaad4f8097139096f69055
kobbejager <wfremout+github@gmail.com>
2022-02-18 17:56:01 +0100

    Create basemodels for sample and collection

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,3 +1,5 @@
 from .template import Template, TemplateUpdate, TemplateList
 from .project import Project, ProjectUpdate, ProjectList
 from .dataset import Dataset, DatasetUpdate, DatasetList
+from .collection import Collection, CollectionUpdate, CollectionList
+from .sample import Sample, SampleUpdate, SampleList
--- src/models/sample.py
+++ src/models/sample.py
@@ -1,1 +1,29 @@
-from pydantic import BaseModel, Field
+from typing import Optional, List, Set
+
+from pydantic import BaseModel, HttpUrl, Field
+
+from models.common import IdBaseModel, LoggingBaseModel, QueryParameters, Terms, Contributor
+
+
+class SampleUpdate(BaseModel):
+    _schema: HttpUrl = Field(...)
+    sample_code: str = Field(..., description='Sample code')
+    parent_sample_code: Optional[str] = Field(None, description='Parent sample code')
+    description: str = Field(..., description='Description of the sample')
+    contributors: Optional[Set[Contributor]] = Field(...)
+    terms: Optional[Terms] = Field(None)
+    persistent_identifier: Optional[str] = Field(None, description='Persistent identifier for the sample')
+
+
+class _SampleShort(SampleUpdate, IdBaseModel):
+    pass
+
+
+class Sample(LoggingBaseModel, _SampleShort):
+    class Config:
+        title = "Generic sample"
+
+
+class SampleList(BaseModel):
+    query_parameters: QueryParameters = Field(...)
+    data: List[_SampleShort] = Field([])
