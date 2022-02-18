54b08a20fe7af426bc266f951b8f4eb8566c1ed7
kobbejager <wfremout+github@gmail.com>
2022-02-18 17:59:31 +0100

    Add CRUD for collections and samples

--- src/crud/__init__.py
+++ src/crud/__init__.py
@@ -1,4 +1,6 @@
+from .base import NoResultsError, NotCreatedError, NotUpdatedError, NotDeletedError, DuplicateKeyError
 from .crud_templates import template
 from .crud_projects import project
 from .crud_datasets import dataset
-from .base import NoResultsError, NotCreatedError, NotUpdatedError, NotDeletedError, DuplicateKeyError
+from .crud_collections import collection
+from .crud_samples import sample
