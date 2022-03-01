af6842f769ddf11e028006b2b5edb390c654d7fe
kobbejager <wfremout+github@gmail.com>
2022-03-01 20:53:50 +0100

    Add crud methods for retrieving a single dataset or project by its keys

--- src/crud/crud_datasets.py
+++ src/crud/crud_datasets.py
@@ -1,4 +1,17 @@
-from crud.base import CRUDBase
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+from crud.base import CRUDBase, NoResultsError, translate_from_mongo
 
 
-dataset = CRUDBase()
+class CRUDDataset(CRUDBase):
+    async def get_by_keys(
+            self, 
+            collection: AsyncIOMotorCollection, 
+            *,
+            dataset_code: str) -> dict:
+        result = await collection.find_one({
+            "dataset_code": dataset_code})
+        if result is None: raise NoResultsError
+        return translate_from_mongo(result)
+
+dataset = CRUDDataset()
--- src/crud/crud_projects.py
+++ src/crud/crud_projects.py
@@ -1,4 +1,22 @@
-from crud.base import CRUDBase
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+from crud.base import CRUDBase, NoResultsError, translate_from_mongo
+
+from models.common import Unit
 
 
-project = CRUDBase()
+class CRUDProject(CRUDBase):
+    async def get_by_keys(
+            self, 
+            collection: AsyncIOMotorCollection, 
+            *,
+            project_code: str,
+            unit: Unit) -> dict:
+        result = await collection.find_one({
+            "project_code": project_code, 
+            "unit": unit.value})
+        if result is None: raise NoResultsError
+        return translate_from_mongo(result)
+
+
+project = CRUDProject()
