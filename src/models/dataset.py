a5cb572d99c624a7eff64fe7da1782544123957e
kobbejager <wfremout+github@gmail.com>
2022-03-11 20:00:44 +0100

    Add possibity to unlink a related project

--- src/models/dataset.py
+++ src/models/dataset.py
@@ -11,15 +11,19 @@ class File(BaseModel):
 
 
 class Project(BaseModel):
-    project_id: str = Field(..., title='Project Id')
-    project_code: str = Field(..., description='Project code (file number, acronym...)')
-    unit: Unit = Field(...)
+    project_id: Optional[str] = Field(None, title='Project Id')
+    project_code: Optional[str] = Field(None, description='Project code (file number, acronym...)')
+    unit: Optional[Unit] = Field(None) 
+    # Note:
+    # These properties should actually not be optional, since the project-property in a Dataset is
+    # itself optional. However, due to a bug in VJSF (https://github.com/koumoul-dev/vuetify-jsonschema-form/issues/230)
+    # this was done as a temporary workaround
 
 
 class DatasetUpdate(BaseModel):
     _schema: HttpUrl = Field(...)
     dataset_code: str = Field(..., description='Dataset code')
-    project: Project = Field(...)
+    project: Optional[Project] = Field(...)
     object_id: Optional[int] = Field(None, description="Object number to which this dataset belongs")
     contributors: Optional[Set[Contributor]] = Field(...)
     terms: Optional[Terms] = Field(None)
