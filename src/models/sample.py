0370007867ac8cf6ae2ab9a10fc94c92caf066b4
kobbejager <wfremout+github@gmail.com>
2022-03-30 15:39:27 +0200

    Add related projects to sample model

--- src/models/sample.py
+++ src/models/sample.py
@@ -2,7 +2,7 @@ from typing import Optional, List, Set
 
 from pydantic import BaseModel, HttpUrl, Field
 
-from models.common import IdBaseModel, LoggingBaseModel, QueryParameters, Terms, Contributor
+from models.common import IdBaseModel, LoggingBaseModel, QueryParameters, Terms, Contributor, Unit
 
 
 class Collection(BaseModel):
@@ -15,11 +15,22 @@ class ParentSample(BaseModel):
     parent_sample_code: Optional[str] = Field(None, description='Parent sample code')
 
 
+class Project(BaseModel):
+    project_id: Optional[str] = Field(None, title='Project Id')
+    project_code: Optional[str] = Field(None, description='Project code (file number, acronym...)')
+    unit: Optional[Unit] = Field(None) 
+    # Note:
+    # These properties should actually not be optional, since the project-property in a Dataset is
+    # itself optional. However, due to a bug in VJSF (https://github.com/koumoul-dev/vuetify-jsonschema-form/issues/230)
+    # this was done as a temporary workaround
+
+
 class SampleUpdate(BaseModel):
     _schema: HttpUrl = Field(...)
     sample_code: str = Field(..., description='Sample code')
     collection: Collection = Field(...)
     parent_sample: Optional[ParentSample] = Field(None)
+    projects: Optional[Set[Project]] = Field(None)
     description: Optional[str] = Field(None, description='Description of the sample')
     contributors: Optional[Set[Contributor]] = Field(...)
     terms: Optional[Terms] = Field(None)
